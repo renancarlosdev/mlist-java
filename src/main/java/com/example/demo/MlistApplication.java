b64a5603b4a7a1b35e2322e755e5a4b802b6ba51
Renan Rosa <renan.rosa@insignus.local>
2018-10-05 18:02:12 -0300

    segundo commit

--- src/main/java/com/example/demo/MlistApplication.java
+++ src/main/java/com/example/demo/MlistApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MlistApplication {
 
 	public static void main(String[] args) {
+		//teste deploy
 		SpringApplication.run(MlistApplication.class, args);
 	}
 }
